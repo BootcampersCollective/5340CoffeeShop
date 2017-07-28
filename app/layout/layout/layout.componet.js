2f4fc6cdca273afd0b7dda1839637e090cbb9fb2
Wesley Song <wes.song@gmail.com>
2017-07-27 19:57:20 -0600

    setup layout for side bar part2

--- app/layout/layout/layout.componet.js
+++ app/layout/layout/layout.componet.js
@@ -8,13 +8,13 @@ const layoutComponent = {
 		};
 	},
 	template: `<div class="container-main">
-<div class="container-left">
-<header-component></header-component>
-<footer-component></footer-component>
-</div>
-<div class="container-right">
-<body-component></body-component>
-</div>
+    <div class="container-left">
+        <header-component></header-component>
+        <footer-component></footer-component>
+    </div>
+    <div class="container-right">
+        <body-component></body-component>
+    </div>
 </div>`
 };
 
