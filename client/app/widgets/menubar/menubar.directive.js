6c0d5b5a104b74e3706a6ed44c4f5aef9ddb5782
Wesley Song <wes.song@gmail.com>
2017-07-29 12:46:50 -0600

    #10 WIP getting data to pass to button

--- client/app/widgets/menubar/menubar.directive.js
+++ client/app/widgets/menubar/menubar.directive.js
@@ -4,13 +4,34 @@ function coffeeMenuBar() {
 		restrict: 'EA',
 		template: `<div class="menu-bar">
     <h1>Menu Bar</h1>
-    <coffee-menu-button data="button" ng-repeat="button in menuButtons"></div>
+    <coffee-menu-button data="{{button}}" ng-repeat="button in menuButtons"></div>
 </div>`,
 	};
 	return directive;
 
 	function link(scope, element, attrs) {
-		scope.menuButtons = ['one', 'two', 'three'];
+		scope.menuButtons = [
+			{
+				name: 'Home',
+				sref: 'app'
+			},
+			{
+				name: 'About',
+				sref: 'app.about'
+			},
+			{
+				name: 'Events',
+				sref: 'app.events'
+			},
+			{
+				name: 'Menu',
+				sref: 'app.menu'
+			},
+			{
+				name: 'Get Here',
+				sref: 'app.gethere'
+			},
+		];
 
 	}
 }
--- client/app/widgets/menubutton/menubutton.directive.js
+++ client/app/widgets/menubutton/menubutton.directive.js
@@ -3,13 +3,17 @@ function coffeeMenuButton() {
 		link: link,
 		restrict: 'EA',
 		template: `<div class="menu-button">
-    <p>Menu Button {{}}</p>
+    <a ui-sref="{{sref}}">{{name}}</a>
 </div>`,
 	};
 	return directive;
 
 	function link(scope, element, attrs) {
-		console.log('coffeeMenuButton attrs', attrs);
+		console.log('coffeeMenuButton attrs', attrs.data);
+
+		scope.name = attrs.data.name;
+		console.log('coffeeMenuButton scope.name', scope.name);
+		scope.sref = attrs.data.sref;
 	}
 }
 
