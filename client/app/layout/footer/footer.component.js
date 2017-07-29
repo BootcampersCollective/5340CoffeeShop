982c77e0e6c91cc06f6dcbcab3dee8b154812479
Nicole Hall <nhall@pairin.com>
2017-07-29 13:35:09 -0600

    angular fails to compile

--- client/app/layout/footer/footer.component.js
+++ client/app/layout/footer/footer.component.js
@@ -1,9 +1,30 @@
 const footerComponent = {
 	bindings: {},
 	controller: function () {
+		let ctrl = this;
+		var ctrl.storeHours;
+		ctrl.date = new Date();
+		ctrl.hourInfoBasedOnDay = hourInfoBasedOnDay;
 
+		function hourInfoBasedOnDay(ctrl.date){
+			//if it is Saturday
+			if (ctrl.date.getDate() === 6 ) {
+				ctrl.storeHours = "open from 7:30am - 3:00pm";
+			//if it is sunday
+			} else if (ctrl.date.getDate() === 0) {
+				ctrl.storeHours = "closed";
+			} else {
+				ctrl.storeHours = "open from 6:30am - 4pm";
+			}
+		return ctrl.storeHours;
+		}
 	},
-	template: `<div class="container-footer">footer</div>`
+
+	template:`<div class="container-footer">footer
+		          <p> 6474 Ward Rd, Arvada, CO 80004 Today is {{$ctrl.date | date:'EEEE-MM-d'}}
+									Today we are {{$ctrl.stroreHours}}
+							</p>
+	          </div>`
 };
 
 angular.module('5340-site.layout')
