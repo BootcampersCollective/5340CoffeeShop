7c6b9ec1f0e6815aa090120e58651d34473b029b
Wesley Song <wes.song@gmail.com>
2017-07-29 10:35:47 -0600

    #14 scaffold apiService

---
+++ app/common/apiService/api.service.js
@@ -0,0 +1,16 @@
+function apiService() {
+	let variable = {};
+	const service = {
+		method: method
+	};
+	return service;
+
+	function method() {}
+
+	function private() {}
+}
+
+angular.module('app')
+	.factory('apiService', apiService);
+
+apiService.$inject = [];
