a30839e450225ba6dbb1bcc645bfbbfef7c6426d
Trenton Greer <yoshigreer@gmail.com>
2017-07-29 14:22:26 -0600

    added email icon and functionality

--- client/app/widgets/menubar/menubar.directive.js
+++ client/app/widgets/menubar/menubar.directive.js
@@ -7,6 +7,7 @@ function coffeeMenuBar() {
     <coffee-menu-button data="{{button}}" ng-repeat="button in menuButtons"></div>
 		<div><a href="https://www.facebook.com/5340Coffee/"><img src="images/facebook.png" alt="facebook" /></a>
 		<a href="https://twitter.com/40weightarvada?lang=en"><img src="images/twitter.png" alt="twitter" /></a>
+		<a href="mailto:info@40weightcoffee.com"><img src="images/email.png" alt="email" /></a>
 		</div>
 </div>`,
 	};
