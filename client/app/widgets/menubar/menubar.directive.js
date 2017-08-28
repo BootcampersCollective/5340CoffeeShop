0441fc84dc3bee326dd67f217a0b3d7ab278c322
Charlie Beck <charlie@bootcamperscollective.com>
2017-08-28 12:34:21 -0600

    remove menu bar title from menu bar

--- client/app/widgets/menubar/menubar.directive.js
+++ client/app/widgets/menubar/menubar.directive.js
@@ -4,7 +4,6 @@ function coffeeMenuBar() {
 		restrict: 'EA',
 		template: `<div class="menu-bar">
                 <div id="nav-img"></div>
-    <h1>Menu Bar</h1>
     <coffee-menu-button data="{{button}}" ng-repeat="button in menuButtons"></div>
 		<div><a href="https://www.facebook.com/5340Coffee/" target="_blank"><img src="images/facebook.png" alt="facebook" /></a>
 		<a href="https://twitter.com/40weightarvada?lang=en" target="_blank"><img src="images/twitter.png" alt="twitter" /></a>
