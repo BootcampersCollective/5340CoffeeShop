f4d5ad8c3fc4274b2984ef5fb8c39c05393efcbf
RJ Russell <RJ-Russell@users.noreply.github.com>
2017-08-29 19:15:04 -0600

    add ngInject annotation in place of

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -1,9 +1,8 @@
-function bcSiteConfig($locationProvider, $urlRouterProvider){
-	$locationProvider.html5Mode(true);
-	$urlRouterProvider.otherwise('/');
+function bcSiteConfig($locationProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+  $urlRouterProvider.otherwise('/');
 }
 
-angular.module('5340-site')
-	.config(bcSiteConfig);
+angular.module('5340-site').config(bcSiteConfig);
 
 bcSiteConfig.$inject = ['$locationProvider', '$urlRouterProvider'];
--- client/app/events/components/events.component.js
+++ client/app/events/components/events.component.js
@@ -1,39 +1,38 @@
 const events = {
-    bindings: {},
-    controller: function (apiService) {
-        let ctrl = this;
+  bindings: {},
+  controller: /*@ngInject*/ function(apiService) {
+    let ctrl = this;
 
-        ctrl.$onInit = function () {
-            apiService.getEvents()
-                .then(function(res) {
-                    // Store array of JSON events.
-                    ctrl.events = res.data;
-                    console.log(ctrl.events);
-                }, function(data, status) {
-                    console.log("Data: ", data);
-                    console.log("Status: ", status);
-                });
-        };
-    },
-    template: `
-        <h1>Events</h1>
-        <h3>{{ $ctrl.events[0].place.name }}</h3>
-        <h4>
-            {{ $ctrl.events[0].place.location.street }} {{ $ctrl.events[0].place.location.city }}
-            {{ $ctrl.events[0].place.location.state }} {{ $ctrl.events[0].place.location.zip }}
-        </h4>
-        <div>
-            <section ng-repeat="(key, value) in $ctrl.events">
-              <div><b>{{ value.name }}</b></div>
-              <div>Time: {{ value.start_time }} - {{ value.end_time }}</div>
-              <div>Description: {{ value.description }}</div>
-              <hr/>
-            </section>
-        </div>
+    ctrl.$onInit = function() {
+      apiService
+        .getEvents()
+        .then(function(res) {
+          // Store array of JSON events.
+          ctrl.events = res.data;
+          console.log(ctrl.events);
+        })
+        .then(function(data, status) {
+          console.log('Data: ', data);
+          console.log('Status: ', status);
+        });
+    };
+  },
+  template: `
+    <h1>Events</h1>
+    <h3>{{ $ctrl.events[0].place.name }}</h3>
+    <h4>
+        {{ $ctrl.events[0].place.location.street }} {{ $ctrl.events[0].place.location.city }}
+        {{ $ctrl.events[0].place.location.state }} {{ $ctrl.events[0].place.location.zip }}
+    </h4>
+    <div>
+        <section ng-repeat="(key, value) in $ctrl.events">
+            <div><b>{{ value.name }}</b></div>
+            <div>Time: {{ value.start_time }} - {{ value.end_time }}</div>
+            <div>Description: {{ value.description }}</div>
+            <hr/>
+        </section>
+    </div>
   `
 };
 
-angular.module('5340-site.events')
-    .component('events', events);
-
-about.$inject = ['apiService'];
+angular.module('5340-site.events').component('events', events);
