32188f05d991220d022b560dac11c371db69769a
Christopher Justice <justcd@mail.broward.edu>
2017-09-08 14:18:27 -0400

    Add nav image

--- client/app/widgets/menubar/menubar.directive.js
+++ client/app/widgets/menubar/menubar.directive.js
@@ -5,7 +5,7 @@ function coffeeMenuBar() {
 		template: `<div class="menu-bar">
                 <a href="app.home"><div id="nav-img"></div></a>
     <coffee-menu-button data="{{button}}" ng-repeat="button in menuButtons"></div>
-		<div><a href="https://www.facebook.com/5340Coffee/" target="_blank"><img src="images/facebook.png" alt="facebook" /></a>
+		<div class= "social-media-links"><a href="https://www.facebook.com/5340Coffee/" target="_blank"><img src="images/facebook.png" alt="facebook" /></a>
 		<a href="https://twitter.com/40weightarvada?lang=en" target="_blank"><img src="images/twitter.png" alt="twitter" /></a>
 		<a href="mailto:info@40weightcoffee.com" target="_blank"><img src="images/email.png" alt="email" /></a>
 		</div>
@@ -16,10 +16,6 @@ function coffeeMenuBar() {
 
 	function link(scope, element, attrs) {
 		scope.menuButtons = [
-			{
-				name: 'Home',
-				sref: 'app.home'
-			},
 			{
 				name: 'About',
 				sref: 'app.about'
